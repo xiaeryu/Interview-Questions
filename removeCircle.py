7fcffe693b7df1f59781e301611b2a703bc963c8
xiaeryu <xiaeryu@nus.edu.sg>
2015-05-23 12:38:13 +0800

    Update removeCircle.py

--- removeCircle.py
+++ removeCircle.py
@@ -1,3 +1,10 @@
+########################################################################################
+## Problem desctiption: 
+## n numbers (0,1,...,n-1) form a circle. Starting from 0, the m th number is deleted each time.
+## After deleting a number, the next number would be counted as 1st and the deletion goes on.
+## Who will be the last number remained given n and m?
+########################################################################################
+
 import sys
 
 nargs = len(sys.argv)
